6632a8ccf1561420bdad514ba010a51d3be0745f
cgutz85 <56197047+cgutz85@users.noreply.github.com>
2025-04-23 22:18:23 +0200

    Update vector_store_prep.py

--- vector_store_prep.py
+++ vector_store_prep.py
@@ -15,6 +15,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 # Read all the available docx files and append to a list
+# Reference for os.walk: https://www.w3schools.com/python/ref_os_walk.asp
+# Reference for os.path.join: https://www.geeksforgeeks.org/python-os-path-join-method/
 word_docs = []
 for root, dirs, files in os.walk("Testdata"):
    #print(root, dirs, files)
@@ -32,40 +34,43 @@ for root,dirs,files in os.walk("Testdata"):
         if file.endswith(".xlsx"):
             xlsx_docs.append(os.path.join(root, file))
 
-# Append all the documents to the "docs" list to then be able to process them all together
-docs = []
+# Append all the documents to the "documents" list to then be able to process them all together
+documents = []
 for doc in word_docs:
     loader = Docx2txtLoader(doc)
     temp = loader.load()
-    docs.extend(temp)
+    documents.extend(temp)
 
 for doc in xlsx_docs:
     loader = UnstructuredExcelLoader(doc)
     temp = loader.load()
-    docs.extend(temp)
-print(len(docs))
+    documents.extend(temp)
+print(len(documents))
     # print(temp)
     # break
 
 def number_documents():
-    len(docs)
+    len(documents)
 # Separate the text from documents into chunks for the vector database
+#Reference for text splitter code: https://python.langchain.com/v0.2/docs/tutorials/rag/
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-chunks = text_splitter.split_documents(docs)
+chunks = text_splitter.split_documents(documents)
 #print(chunks)
 
+# Referemce for tiktoken: https://cookbook.openai.com/examples/how_to_count_tokens_with_tiktoken
 encoding = tiktoken.encoding_for_model("gpt-4o-mini")
 
 
 embeddings = OllamaEmbeddings(model='mxbai-embed-large', base_url='http://localhost:11434')
 
+# Reference for vector store initialization: https://github.com/langchain-ai/langchain/discussions/25376
 vector = embeddings.embed_query("Hello World")
-
 index = faiss.IndexFlatL2(len(vector))
 #print(index.ntotal)
 #print(index.d)
 
 # Definition of the vector store
+# Reference for FAISS vector store: https://python.langchain.com/docs/integrations/vectorstores/faiss/
 vector_store = FAISS(
     embedding_function=embeddings,
     index=index,
@@ -73,6 +78,7 @@ vector_store = FAISS(
     index_to_docstore_id={},
 )
 
+# Reference for debugging check: https://stackoverflow.com/questions/70624600/faiss-how-to-retrieve-vector-by-id-from-python
 print(vector_store.index.ntotal, vector_store.index.d)
 
 
